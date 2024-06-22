25f0d39bc274dc529a2dc990e3674455128cec8a
Team2Guys <ahmed.saddique12@gmail.com>
2024-06-22 19:05:44 +0500

    heero

--- src/components/Brand/index.tsx
+++ src/components/Brand/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useState } from 'react';
-import img from '@images/demo.jpg';
+import img from '@images/paint.jpg';
 import Image from 'next/image';
 
 type TabContent = {
@@ -18,25 +18,25 @@ const Brands: React.FC = () => {
     tab1: {
       title: 'MONSTER SEALANT',
       description:
-        'Introducing Richmond Monster Sealants, your trusted partner in superior flooring solutions...',
+        'Lorem Ipsum when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha survived not only five centuries, but also the leap inpesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
       image: img,
     },
     tab2: {
       title: 'NON-FORMALDEHYDE FLOORING',
       description:
-        'Discover our non-formaldehyde flooring solutions for a healthier home...',
+        'when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha survived not only five centuries, but also the leap into elctronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
       image: img,
     },
     tab3: {
       title: 'PLUVIA FLOORING',
       description:
-        'Explore the durability and elegance of Pluvia Flooring for all your flooring needs...',
+        'Explore the durabwhen an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha survived not only five centuries, but also the leap into electronic typesetti unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsumility and elegance of Pluvia Flooring for all your flooring needs...',
       image: img,
     },
     tab4: {
       title: 'STONE TEC',
       description:
-        'Experience the strength and beauty of Stone Tec in your home...',
+        'when when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha survived not only five centuries, but also the leap intoelectronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem IpsumExperience the strength and beauty of Stone Tec in your home...',
       image: img,
     },
   };
@@ -48,9 +48,8 @@ const Brands: React.FC = () => {
           <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
             OUR BRANDS!
           </h2>
-          <p className="mt-3 text-lg text-gray-500 sm:mt-4">
-            BUILDING GENERATIONS OF QUALITY, FLOORING, STONE, & MORE. A UK
-            FAMILY BUSINESS SERVING YOU SINCE 2001.
+          <p className="mt-3 text-lg sm:mt-4">
+          Lorem Ipsum when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha survived not only five centuries, but also the leap inpesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
           </p>
         </div>
 
@@ -96,10 +95,20 @@ const Brands: React.FC = () => {
             >
               Stone Tec
             </button>
+            <button
+              onClick={() => setActiveTab('tab4')}
+              className={`px-2 md:px-4 py-2 font-medium text-xs border-2 border-black md:text-sm rounded-full ${
+                activeTab === 'tab5'
+                  ? 'bg-black text-white'
+                  : 'bg-white  text-black'
+              }`}
+            >
+              Delux Stone
+            </button>
           </div>
 
           <div className="mt-10">
-            <div className="flex flex-col lg:flex-row items-center lg:items-start lg:justify-between">
+            <div className="flex  items-center  bg-gray-300 py-10 px-10">
               <div className="lg:w-1/2">
                 <Image
                   className="h-72 w-full object-cover sm:h-96 md:h-full lg:w-full lg:h-full"
@@ -110,12 +119,17 @@ const Brands: React.FC = () => {
                 />
               </div>
               <div className="mt-6 lg:mt-0 lg:ml-10 lg:w-1/2">
-                <h3 className="text-2xl font-extrabold text-gray-900 sm:text-3xl">
+                <h3 className="text-xl font-bold text-start">
                   {tabContent[activeTab].title}
                 </h3>
-                <p className="mt-3 text-base text-gray-500 sm:mt-4">
+                <p className="mt-3 text-base  sm:mt-4">
                   {tabContent[activeTab].description}
                 </p>
+                <div className='flex gap-2 mt-5'>
+                  <button className='bg-black rounded-full text-xs md:text-base  md:px-4 p-2 text-white'>Download Now</button>
+                  <button className='bg-black rounded-full text-xs md:text-base  md:px-4 p-2 text-white'>Read More</button>
+                  <button className='bg-black rounded-full text-xs md:text-base  md:px-4 p-2 text-white'>Visit Official Site</button>
+                </div>
               </div>
             </div>
           </div>
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import img from '@images/demo.jpg';
 import Image from 'next/image';
+import Link from 'next/link';
 const Hero: React.FC = () => {
   return (
     <div className="bg-gray-300 max-w-7xl mx-auto md:mt-10 flex flex-col lg:flex-row items-center pt-5 pb-5 px-10 lg:justify-between ">
       <div className="text-center lg:text-left lg:max-w-lg">
         <h1 className=" tracking-tight">
           <span className="block text-lg">WELCOME TO</span>
-          <span className="block text-2xl">AGSONS MIDDLE EAST LLC</span>
+          <span className="block text-2xl font-bold">AGSONS MIDDLE EAST LLC</span>
         </h1>
-        <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl md:mt-5 md:text-xl lg:mx-0">
+        <p className="mt-3 text-base sm:mt-5  sm:max-w-xl md:mt-5  lg:mx-0">
           At AGSONS Middle East, we’re proud to be an extension of the UK family
           business, bringing generations of experience and excellence to the
           region.
         </p>
         <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
           <div className="rounded-md shadow">
-            <a
-              href="#"
-              className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+            <Link
+              href={"/"}
+              className="w-full px-4 py-3 border border-transparent text-base font-medium rounded-full text-white bg-black "
             >
               Read More
-            </a>
+            </Link>
           </div>
         </div>
       </div>
